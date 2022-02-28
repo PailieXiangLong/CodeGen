db121981f5dd73ba2a9a0c486fb98600aa14c2e4
PailieXiangLong <xianglong.cheng@ecreditpal.com>
2022-02-28 17:59:09 +0800

    更新默认加载模板

--- src/main/java/com/github/hykes/codegen/constants/Defaults.java
+++ src/main/java/com/github/hykes/codegen/constants/Defaults.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 /**
  * 内置参数
+ *
  * @author hehaiyangwork@gmail.com
  * @date 2017/4/16
  */
@@ -41,28 +42,110 @@ public class Defaults {
     /**
      * 获取默认的模板
      */
-    public static List<CodeRoot> getDefaultTemplates(){
+    public static List<CodeRoot> getDefaultTemplates() {
         // 设置默认分组和模板
         List<CodeGroup> groups = new ArrayList<>();
         try {
-            List<CodeTemplate> templates1 = new ArrayList<>();
-            templates1.add(new CodeTemplate(UUID.randomUUID().toString(), "Model", "java", "${model}", FileUtil.loadTextAndClose(Defaults.class.getResourceAsStream("/templates/ModelTemplate.vm")), "model", false));
-            templates1.add(new CodeTemplate(UUID.randomUUID().toString(), "Service", "java", "${model}Service", FileUtil.loadTextAndClose(Defaults.class.getResourceAsStream("/templates/ServiceTemplate.vm")), "service", false));
-            CodeGroup modelGroup = new CodeGroup(UUID.randomUUID().toString(), "model-service", 1, templates1);
+            // models
+            List<CodeTemplate> models = new ArrayList<>();
+            CodeTemplate modelCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Model", "java", "${model}",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/ModelTemplate.vm")
+                    ),
+                    "model", false);
+            models.add(modelCodeTemplate);
+            CodeGroup modelGroup = new CodeGroup(UUID.randomUUID().toString(), "model", 1, models);
             groups.add(modelGroup);
 
-            List<CodeTemplate> templates2 = new ArrayList<>();
-            templates2.add(new CodeTemplate(UUID.randomUUID().toString(), "Dao", "java", "${model}Dao", FileUtil.loadTextAndClose(Defaults.class.getResourceAsStream("/templates/DaoTemplate.vm")), "dao", false));
-            templates2.add(new CodeTemplate(UUID.randomUUID().toString(), "Service", "java", "${model}ServiceImpl", FileUtil.loadTextAndClose(Defaults.class.getResourceAsStream("/templates/ServiceImplTemplate.vm")), "service", false));
-            templates2.add(new CodeTemplate(UUID.randomUUID().toString(), "Mapper", "xml", "${model}Mapper", FileUtil.loadTextAndClose(Defaults.class.getResourceAsStream("/templates/MapperTemplate.vm")), "mapper", true));
-            CodeGroup implGroup = new CodeGroup(UUID.randomUUID().toString(), "service-impl-dao", 2,  templates2);
+            // service
+            List<CodeTemplate> service = new ArrayList<>();
+            CodeTemplate serviceCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Service", "java", "${model}Service",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/ServiceTemplate.vm")
+                    ),
+                    "service", false);
+            service.add(serviceCodeTemplate);
+            CodeGroup serviceGroup = new CodeGroup(UUID.randomUUID().toString(), "service", 2, service);
+            groups.add(serviceGroup);
+
+            // serviceImpl
+            List<CodeTemplate> serviceImpl = new ArrayList<>();
+            CodeTemplate serviceImplCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Service",
+                    "java",
+                    "${model}ServiceImpl",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/ServiceImplTemplate.vm")
+                    ),
+                    "service", false);
+            serviceImpl.add(serviceImplCodeTemplate);
+            CodeGroup serviceImplGroup = new CodeGroup(UUID.randomUUID().toString(), "serviceImpl", 3, serviceImpl);
+            groups.add(serviceImplGroup);
+
+
+            // dao层
+            List<CodeTemplate> daos = new ArrayList<>();
+            CodeTemplate daoCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Dao",
+                    "java",
+                    "${model}Dao",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/DaoTemplate.vm")
+                    ), "dao", false);
+
+            daos.add(daoCodeTemplate);
+            CodeTemplate mapperCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Mapper",
+                    "xml",
+                    "${model}Mapper",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/MapperTemplate.vm")
+                    ),
+                    "mapper", true);
+            daos.add(mapperCodeTemplate);
+            CodeGroup implGroup = new CodeGroup(UUID.randomUUID().toString(), "dao", 4, daos);
             groups.add(implGroup);
 
-            List<CodeTemplate> templates3 = new ArrayList<>();
-            templates3.add(new CodeTemplate(UUID.randomUUID().toString(), "Controller", "java", "${model}s", FileUtil.loadTextAndClose(Defaults.class.getResourceAsStream("/templates/ControllerTemplate.vm")), "front", false));
-            CodeGroup apiGroup = new CodeGroup(UUID.randomUUID().toString(), "api", 2,  templates3);
+            // Controller
+            List<CodeTemplate> apis = new ArrayList<>();
+            CodeTemplate apiCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Controller",
+                    "java",
+                    "${model}Controller",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/ControllerTemplate.vm")
+                    ),
+                    "controller", false);
+            apis.add(apiCodeTemplate);
+            CodeGroup apiGroup = new CodeGroup(UUID.randomUUID().toString(), "api", 5, apis);
             groups.add(apiGroup);
-        } catch (Exception e){
+
+            // page分页工具
+            List<CodeTemplate> pages = new ArrayList<>();
+            CodeTemplate pageCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "Page",
+                    "java",
+                    "Page",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/PageTemplate.vm")
+                    ),
+                    "util", false);
+            pages.add(pageCodeTemplate);
+            CodeTemplate pageParamCodeTemplate = new CodeTemplate(UUID.randomUUID().toString(),
+                    "PageParam",
+                    "java",
+                    "PageParam",
+                    FileUtil.loadTextAndClose(
+                            Defaults.class.getResourceAsStream("/templates/PageParamTemplate.vm")
+                    ),
+                    "util", false);
+            pages.add(pageParamCodeTemplate);
+            CodeGroup pagesGroup = new CodeGroup(UUID.randomUUID().toString(), "page", 6, pages);
+            groups.add(pagesGroup);
+
+        } catch (Exception e) {
             LOGGER.error(StringUtils.getStackTraceAsString(e));
         }
         // 设置默认根, 名称为root
